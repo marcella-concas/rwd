b0e132be52bfe0deb3363e37a0839239b6662ba9
marcella-concas <marcellinabumbum@gmail.com>
2017-05-23 20:19:13 +0200

    Add files via upload

---
+++ rwd/js/main.js
@@ -0,0 +1,6 @@
+
+var lorem = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque lacinia sapien vitae tortor varius congue. Aliquam luctus fringilla aliquam. Aenean euismod ex vel metus ultricies, at finibus eros volutpat. Nullam turpis est, convallis non dolor quis, fringilla dictum justo. Pellentesque sagittis mollis sagittis. Etiam sagittis tempus lectus hendrerit dapibus. Aliquam quis aliquam augue.");lorem = lorem+ lorem;
+lorem=lorem+lorem+lorem+lorem;
+$('.lorem1').html(lorem);
+$('.lorem2').html(lorem);
+$('.lorem3').html(lorem);
